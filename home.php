19e8dad17ff02a97023d91847183a5635208b54a
ChristopherSemard <christophersemard76@gmail.com>
2022-05-23 16:30:12 +0200

    structure home

---
+++ home.php
@@ -0,0 +1,19 @@
+<section class="presentation">
+    <h1 class="presentation_title"></h1>
+    <p class="presentation_text"></p>
+    <img class="presentation_image" src="" alt="" />
+</section>
+
+<section class="partners">
+    <h2 class="partners_title"></h2>
+    <p class="partners_intro"></p>
+
+    <article class="partner">
+        <img class="partner_image" src="" alt="" />
+        <section class="partner_about">
+            <h3 class="partner_title"></h3>
+            <p class="partner_excerpt"></p>
+            <button class="partner_read_more"></button>
+        </section>
+    </article>
+</section>
